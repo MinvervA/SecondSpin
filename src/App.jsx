a1490493453f3eb7129e084dff20de935e7b8dfd
MinervA <andrean923@gmail.com>
2024-10-12 01:05:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-8xl font-bold">SecondSpin</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,3 @@
+export const LandingPage = () => {
+  return <div className="">Landing page</div>;
+};
